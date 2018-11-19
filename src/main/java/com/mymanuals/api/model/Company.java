697a40db80f9712bb4ac6e4c167361593e48f99d
sangeeta2183 <sangeetananda2183@gmail.com>
2018-11-19 22:46:09 +0100

    Added Company and user DB entites

--- src/main/java/com/mymanuals/api/model/Company.java
+++ src/main/java/com/mymanuals/api/model/Company.java
@@ -1,5 +1,30 @@
 package com.mymanuals.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String desc;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
 
 }
