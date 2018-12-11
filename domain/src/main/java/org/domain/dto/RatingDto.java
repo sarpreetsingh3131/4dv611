e68e703cb3cb3f2eedce80994ef230eec16b0ba2
Sarpreet Singh <sb223ce@student.lnu.se>
2018-12-11 19:38:24 +0100

    refactoring

--- domain/src/main/java/org/domain/dto/RatingDto.java
+++ domain/src/main/java/org/domain/dto/RatingDto.java
@@ -2,12 +2,16 @@ package org.domain.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class RatingDto {
 
     @NotNull(message = "Product rating is missing")
+    @Min(value = 1, message = "Product rating must be >= 1")
+    @Max(value = 5, message = "Product rating must be <= 5")
     private Integer rating;
 
     @NotNull(message = "Product's manual id is missing")
